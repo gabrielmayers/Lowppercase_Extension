d4ad23fe4c92239a99530e9b286e02a9fcfd4931
gabrielmayers <gabriel.mayers@hotmail.com>
2020-10-24 14:02:50 -0300

    Lower Added

--- extension/script.js
+++ extension/script.js
@@ -2,8 +2,8 @@
 
 document.addEventListener('DOMContentLoaded', function() {
 
-    // Make it happen just when click on the button:
-    document.querySelector('#btn').addEventListener('click', function () {
+    // Make it happen just when click on the Upper button:
+    document.querySelector('#btnUpper').addEventListener('click', function () {
         
         // Get value from text:
         let text = document.querySelector('#text').value;
@@ -12,9 +12,24 @@ document.addEventListener('DOMContentLoaded', function() {
         document.querySelector("#result").innerHTML = upper(text);
     });
 
+    // Make it happen just when click on the Lower button:
+    document.querySelector("#btnLower").addEventListener('click', function() {
+
+        //Get value from text:
+        let text = document.querySelector('#text').value;
+
+        // Using <div> to show the result:
+        document.querySelector('#result').innerHTML = lower(text)
+    });
+
     // Function to make text Uppercase:
 
     function upper(text) {
         return text.toUpperCase();
+    }
+
+    // Function to make text Lowercase:
+    function lower(text) {
+        return text.toLowerCase();
     }
 });
